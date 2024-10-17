ffebbd2e34c315bdf828f5a73d6e7b01997fb26e
Rustam-bit <mirzaev.ra@phystech.edu>
2024-10-17 08:20:48 +0300

    OOP

--- 1/src/Main.java
+++ 1/src/Main.java
@@ -1,15 +1,35 @@
+import java.util.Arrays;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        System.out.println(Matrix.NEO);
+        System.out.println(Matrix.TRINITY);
+    }
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+     static class Matrix {
+        public static DBObject NEO = new User().initializeIdAndName(1, "Neo");
+        public static DBObject TRINITY = new User().initializeIdAndName(2, "Trinity");
+    }
+
+    interface DBObject {
+        DBObject initializeIdAndName(long id, String name);
+    }
+
+      static class User implements DBObject {
+        long id;
+        String name;
+        public User initializeIdAndName(long id, String name) {
+            this.id = id;
+            this.name = name;
+            return this;
+        }
+
+        @Override
+        public String toString() {
+            return String.format("The user's name is %s, id = %d", name, id);
         }
     }
+
 }
