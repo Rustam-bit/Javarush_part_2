0b16a31c66f4d12ac399e023876b95f307c47201
Rustam-bit <mirzaev.ra@phystech.edu>
2024-10-17 21:21:50 +0300

    Бабка за репу и тд(Задача про сказку)

---
+++ 1/src/Solution_5.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution_5 {
+
+    public static void main(String[] args) {
+        List<Person> plot = new ArrayList<Person>();
+        plot.add(new Person("Репка", "Репку"));
+        plot.add(new Person("Дедка", "Дедку"));
+        plot.add(new Person("Бабка", "Бабку"));
+        plot.add(new Person("Внучка", "Внучку"));
+        RepkaStory.tell(plot);
+    }
+}
