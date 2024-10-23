722dc7db3d83cee0b1e99ac82d9ec25df96d4c9d
Rustam-bit <mirzaev.ra@phystech.edu>
2024-10-23 22:52:12 +0300

    Horse Racing(Multiprogramming)

---
+++ First part/src/Second_part/Solution.java
@@ -0,0 +1,45 @@
+package src.Second_part;
+
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String url = reader.readLine();
+        String paramsSubString = url.substring(url.lastIndexOf("?") + 1);
+        String[] splitParams = paramsSubString.split("&");
+        String objValue = null;
+
+        StringBuilder resultBuilder = new StringBuilder();
+        for (String splitParam : splitParams) {
+            String[] paramAndValue = splitParam.split("=");
+            resultBuilder.append(paramAndValue[0]);
+            resultBuilder.append(" ");
+
+            if (paramAndValue[0].equals("obj")) {
+                objValue = paramAndValue[1];
+            }
+        }
+
+        System.out.println(resultBuilder.toString().trim());
+
+        if (objValue != null) {
+            try {
+                alert(Double.parseDouble(objValue));
+            } catch (NumberFormatException nfe) {
+                alert(objValue);
+            }
+        }
+    }
+
+    public static void alert(double value) {
+        System.out.println("double: " + value);
+    }
+
+    public static void alert(String value) {
+        System.out.println("String: " + value);
+    }
+}
